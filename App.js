d1ffbe2f1d4491afb9fb0bc718f78dd1a600d8fb
Christian Traxler <traxler.lc@gmail.com>
2021-01-16 18:47:43 -0500

    firest commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StartPage from './components/StartPage.js';
+import SignUp from './components/SignUp.js';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' >
+        <Stack.Screen name='Home' component={StartPage} options={{headerShown: false}}/>
+        <Stack.Screen name='SignUp' component={SignUp} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
