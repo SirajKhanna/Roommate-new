ada2b802ae2804103a41d64d5d21825e9b5a094c
Christian Traxler <traxler.lc@gmail.com>
2021-01-16 19:00:41 -0500

    commit

--- components/StartPage.js
+++ components/StartPage.js
@@ -31,7 +31,6 @@ const StartPage = ({navigation}) => {
   const [email, onChangeEmail] = React.useState('Enter your Email Here');
   const [password, onChangePassword] = React.useState('');
   
-
   return (
     <>
       <StatusBar barStyle="dark-content" />
